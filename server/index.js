1cd0f3f6133153cfd8ce9d7fe899201a6a312f67
Maulana Adam Sahid <masadamsahid@gmail.com>
2021-10-16 21:35:52 +0700

    Complete

--- server/index.js
+++ server/index.js
@@ -2,6 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js'
+
+const  app = express();
+dotenv.config()
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(
+    process.env.CONNECTION_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => app.listen(PORT, () => {console.log(`Server successfully running on ${PORT}`)} ))
+    .catch((error)=> {console.log(error.message)});
+
 
 
 
